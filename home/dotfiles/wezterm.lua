99df90e6adc6a7c01f2314b108b5f792edc8a2eb
Ricky06202 <ricardosanjurg@gmail.com>
2026-08-23 17:34:23 -0500

    fix wezterm: suprimir notificaciones vacias (OSC 9/777 via notification_handling)

--- home/dotfiles/wezterm.lua
+++ home/dotfiles/wezterm.lua
@@ -14,4 +14,10 @@ config.line_height = 1.15
 config.window_background_opacity = 0.88
 config.macos_window_background_blur = 0
 
+-- Suprime los "toasts" que WezTerm genera al recibir secuencias OSC 9/777.
+-- Alguna app dentro de la terminal (zellij/opencode) emite secuencias VACÍAS
+-- y se veían como burbujas de notificación sin texto. Valores posibles:
+--   AlwaysShow (default), SuppressFromFocusedPane/Tab/Window, NeverShow.
+config.notification_handling = 'NeverShow'
+
 return config
