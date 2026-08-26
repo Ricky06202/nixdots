0f77647b3fc3d72e0e9f6c5dc6167712ad783647
Ricky06202 <ricardosanjurg@gmail.com>
2026-08-26 00:15:35 -0500

    fix: activar backupFileExtension para home-manager (mimeapps.list existente)

--- flake.nix
+++ flake.nix
@@ -82,6 +82,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "hm-bak";
             home-manager.extraSpecialArgs = { hostName = name; };
             home-manager.users.ricky = import ./home;
           }
